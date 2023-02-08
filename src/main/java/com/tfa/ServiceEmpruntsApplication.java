f2590c9adbab0e635ccd610ef2b79af46971ee46
TIEMBOLOHO FANNY <fannytiem2@yahoo.fr>
2023-02-08 16:10:26 -0500

    First commit

---
+++ src/main/java/com/tfa/ServiceEmpruntsApplication.java
@@ -0,0 +1,13 @@
+package com.tfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmpruntsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEmpruntsApplication.class, args);
+	}
+
+}
